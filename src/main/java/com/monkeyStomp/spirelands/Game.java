c991356047f793674c43ed45fb8bbfb51635bbee
amcnulty <amcnulty88@swbell.net>
2018-08-28 19:20:15 -0700

    Gradle Project
    Started a basic gradle project.
    Program is simply printing Hello World.

---
+++ src/main/java/com/monkeyStomp/spirelands/Game.java
@@ -0,0 +1,15 @@
+package com.monkeyStomp.spirelands;
+/**
+ *
+ * @author Aaron Michael McNulty
+ */
+public class Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+    }
+
+}
