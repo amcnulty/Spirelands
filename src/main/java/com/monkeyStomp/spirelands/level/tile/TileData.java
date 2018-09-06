9aee74c95e1ca2af003710e8b5bf2c7b3c2b2320
amcnulty <amcnulty88@swbell.net>
2018-09-05 17:12:53 -0700

    New Tiles
    Added some new tiles and a new level for testing.

--- src/main/java/com/monkeyStomp/spirelands/level/tile/TileData.java
+++ src/main/java/com/monkeyStomp/spirelands/level/tile/TileData.java
@@ -14,10 +14,26 @@ public class TileData {
   public static HashMap<Integer, ArrayList<Object>> library = new HashMap<>();
   
   static {
-    library.put(0xFF57D357, new ArrayList<>(Arrays.asList(0, 0, SpriteSheet.smallTestSheet, false)));
-    library.put(0xFFC8814F, new ArrayList<>(Arrays.asList(1, 0, SpriteSheet.smallTestSheet, false)));
+    // Grass
+      // Plain
+      library.put(0xFF57D357, new ArrayList<>(Arrays.asList(0, 0, SpriteSheet.smallTestSheet, false)));
+      // Flower
+      library.put(0xFFF1F905, new ArrayList<>(Arrays.asList(2, 0, SpriteSheet.smallTestSheet, false)));
+      // Rock
+      library.put(0xFF807854, new ArrayList<>(Arrays.asList(2, 1, SpriteSheet.smallTestSheet, true)));
+    // Dirt
+      // Plain
+      library.put(0xFFC8814F, new ArrayList<>(Arrays.asList(1, 0, SpriteSheet.smallTestSheet, false)));
+      // Rock
+      library.put(0xFF6D433C, new ArrayList<>(Arrays.asList(2, 2, SpriteSheet.smallTestSheet, true)));
+    // Sand
     library.put(0xFFF3E191, new ArrayList<>(Arrays.asList(0, 1, SpriteSheet.smallTestSheet, false)));
-    library.put(0xFF00E6E6, new ArrayList<>(Arrays.asList(1, 1, SpriteSheet.smallTestSheet, false)));
+    // Water
+    library.put(0xFF00E6E6, new ArrayList<>(Arrays.asList(1, 1, SpriteSheet.smallTestSheet, true)));
+    // Cobblestone
+    library.put(0xFF9B9B91, new ArrayList<>(Arrays.asList(0, 2, SpriteSheet.smallTestSheet, false)));
+    // Grey Brick
+    library.put(0xFF5B6A72, new ArrayList<>(Arrays.asList(1, 2, SpriteSheet.smallTestSheet, true)));
   }
 
 }
