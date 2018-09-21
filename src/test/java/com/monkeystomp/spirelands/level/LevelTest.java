144b83d9bfe0650044b861b5ed4d4f03b57eb4b6
amcnulty <amcnulty88@swbell.net>
2018-09-20 18:05:05 -0700

    Test Packages
    Created actual test packages that mirror the package structure of the application.
    Wrote a test around they entityHere method in the Entity class.

---
+++ src/test/java/com/monkeystomp/spirelands/level/LevelTest.java
@@ -0,0 +1,11 @@
+package com.monkeystomp.spirelands.level;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author Aaron Michael McNulty
+ */
+public class LevelTest {
+}
